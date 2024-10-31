152ded12ac8c0902c7ec3106af404af9fd5b399c
mrvalraven <akatiago@gmail.com>
2024-10-31 01:42:01 +0000

    made game functional

--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -5,11 +5,17 @@ import daresFile from '../../static/dares.json';
 import truthsFile from '../../static/truths.json';
 import usePlayers from "../../hooks/usePlayers";
 
+const gamemodeTitles = {
+}
+
 const Questions = () => {
   const { players, setPlayers } = usePlayers();
   const [currentPlayers, setCurrentPlayers] = useState(players)
+  const [status, setStatus] = useState("modes")
   const [dares, setDares] = useState([]);
   const [truths, setTruths] = useState([]);
+  const [daresCounter, setDaresCounter] = useState(0);
+  const [truthsCounter, setTruthsCounter] = useState(0);
   const [currentChallenge, setCurrentChallenge] = useState("")
   const [currentChallengeMode, setCurrentChallengeMode] = useState("")
   const [currentPlayer, setCurrentPlayer] = useState({
@@ -19,8 +25,7 @@ const Questions = () => {
     sexuality: "",
   });
   const { mode } = useParams();
-
-  const selectedRandomPlayer = (playersArr) => {
+  const selectRandomPlayer = (playersArr) => {
     const randomIndex = Math.floor(playersArr.length * Math.random());
     return playersArr[randomIndex];
   }
@@ -28,7 +33,9 @@ const Questions = () => {
   const chooseCompatiblePlayer = (currentPlayer) => {
     const playersArrayWithoutCurrentPlayer = players.filter((currentPlayerObj) => currentPlayerObj.id !== currentPlayer.id);
 
-    return playersArrayWithoutCurrentPlayer;
+
+
+    return selectRandomPlayer(playersArrayWithoutCurrentPlayer);
   }
 
   const getRandomIndex = (array) => {
@@ -36,35 +43,53 @@ const Questions = () => {
   }
 
   const handleClick = (challengeType) => {
-    console.log('handleClick', challengeType)
+    setStatus('challenges');
     if (challengeType === 'truth') {
-      setCurrentChallenge(truths[getRandomIndex(truths)]);
+      const truthChallenge = truths[getRandomIndex(truths)].replace('-player-', chooseCompatiblePlayer(currentPlayer).name)
+      setCurrentChallenge(`${currentPlayer.name}, ${truthChallenge}`);
       setCurrentChallengeMode('truth');
+      setTruthsCounter((previousValue) => previousValue + 1);
       return;
     }
 
-    setCurrentChallenge(dares[getRandomIndex(dares)]);
+    const dareChallenge = dares[getRandomIndex(dares)].replace('-player-', chooseCompatiblePlayer(currentPlayer).name);
+
+    setCurrentChallenge(`${currentPlayer.name}, ${dareChallenge}`);
     setCurrentChallengeMode('dare');
+    setDaresCounter((previousValue) => previousValue + 1);
+  }
+
+  const handleNextChallenge = () => {
+    setStatus('modes');
+    setCurrentPlayer(selectRandomPlayer(players));
   }
 
   useEffect(() => {
     setDares(daresFile[mode.trim()]);
     setTruths(truthsFile[mode.trim()]);
-    setCurrentPlayer(selectedRandomPlayer(players));
+    setCurrentPlayer(selectRandomPlayer(players));
   }, []);
 
   return (
     <section className="questions">
-      <div className="modes">
-        <div className="modeButton" onClick={() => handleClick('truth')}>Truth</div>
-        <div>{currentPlayer?.name || 'Random'}</div>
-        <div className="modeButton" onClick={() => handleClick('dare')}>Dare</div>
-      </div>
-      <div className="currentChallenge">
-        <span>{ } / {currentChallengeMode === 'truth' ? truths.length : dares.length}</span>
-        <p>Current: {currentChallenge}</p>
-      </div>
-
+      {status === 'modes' ?
+        <div className="modes">
+          <h1>Playing </h1>
+          <h2>It's {currentPlayer?.name || 'Random'}'s turn!</h2>
+          <div className="modeButton truthBtn" onClick={() => handleClick('truth')}>Truth</div>
+          <div className="modeButton" onClick={() => handleClick('dare')}>Dare</div>
+        </div>
+        :
+        <div className="currentChallenge">
+          <span>{currentChallengeMode === 'truth' ?
+            truthsCounter + ' / ' + truths.length :
+            daresCounter + ' / ' + dares.length}
+          </span>
+          <h1>{currentChallengeMode}</h1>
+          <p>{currentChallenge}</p>
+          <button onClick={handleNextChallenge}>Next!</button>
+        </div>
+      }
     </section>
   );
 }
